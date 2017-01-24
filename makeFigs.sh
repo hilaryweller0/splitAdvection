303eb306ae5594eaf0525b2b74fc401e79c43cd8
Hilary Weller <h.weller@reading.ac.uk>
2017-01-24 16:22:19 +0000

    Version submitted to QJRMS and to arxiv

--- makeFigs.sh
+++ makeFigs.sh
@@ -12,3 +12,10 @@ done
 cd ..
 
 cp -u HilarysGraphics/solidBodyRotationOnPlane_nonOrthog_50x50_analytic_constant_mesh.pdf .
+
+eps2png2 deform.pdf
+eps2png2 sbrc10.pdf
+eps2png2 sbrc1.pdf
+
+zip figures.zip stencil.pdf solidBodyRotationOnPlane_nonOrthog_50x50_analytic_constant_mesh.pdf sbrc1.png sbrc10.png sbr_dx.pdf sbr_dt.pdf overMountains.png overMountains_dx.pdf deform_init.pdf deform.png deform_dx.pdf deform_dt.pdf
+
